7b9a0a5a23a972f6d1dff0b82fa115028882883d
beanstown106 <nbenis106@gmail.com>
2015-05-03 03:50:51 -0400

    Build Google audio files from android12 [1/2]
    Change-Id: Ib20341d418c9f827ab86e60d52724d96b8d7e31a
    Signed-off-by: Jason Edson <jaysonedson@gmail.com>

--- target/product/full_base.mk
+++ target/product/full_base.mk
@@ -38,18 +38,14 @@ PRODUCT_PACKAGES += \
 
 # Additional settings used in all AOSP builds
 PRODUCT_VENDOR_PROPERTIES := \
-    ro.config.ringtone?=Ring_Synth_04.ogg \
-    ro.config.notification_sound?=pixiedust.ogg
+    ro.config.ringtone?=Your_new_adventure.ogg \
+    ro.config.notification_sound?=Eureka.ogg
 
 # Put en_US first in the list, so make it default.
 PRODUCT_LOCALES := en_US
 
 # Get some sounds
-ifeq ($(DERP_BUILD),)
-$(call inherit-product-if-exists, frameworks/base/data/sounds/AllAudio.mk)
-else
-$(call inherit-product-if-exists, frameworks/base/data/sounds/AudioPackage14.mk)
-endif
+$(call inherit-product-if-exists, frameworks/base/data/sounds/GoogleAudio.mk)
 
 # Get a list of languages.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
