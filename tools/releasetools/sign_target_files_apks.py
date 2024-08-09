5db64f471d1bc66b311689be2c7e25173f71bb46
Dhina17 <dhinalogu@gmail.com>
2024-08-09 14:10:28 +0530

    Revert "releasetools: support prebuilt vendor.img"
    It skips signing vendor image.
    
    This reverts commit 33b4cd4cc1118d75fae18062043bbafab4f59042.
    
    Change-Id: If454d4936ec7889295a315b78a85ce7126bf9caf

--- tools/releasetools/sign_target_files_apks.py
+++ tools/releasetools/sign_target_files_apks.py
@@ -595,7 +595,7 @@ def ProcessTargetFiles(input_tf_zip: zipfile.ZipFile, output_tf_zip, misc_info,
 
   for info in input_tf_zip.infolist():
     filename = info.filename
-    if filename.startswith("IMAGES/") and not filename.endswith("vendor.img"):
+    if filename.startswith("IMAGES/"):
       continue
 
     # Skip OTA-specific images (e.g. split super images), which will be
